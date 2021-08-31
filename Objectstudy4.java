6e994fca705fd917becc039cea7798b435c70f5f
devyoseph <89521661+devyoseph@users.noreply.github.com>
2021-08-31 23:57:11 +0900

    Update Objectstudy4.java

--- Objectstudy4.java
+++ Objectstudy4.java
@@ -1,4 +1,9 @@
 // 일부러 클래스에서 인스턴스로 접근해서 오류내보기 
+
+// 클래스: static  영역
+// 인스턴스: non-static 영역
+// 변수 = field(variable)
+// 클래스 변수, 인스턴스 변수 = static field, instance field
 class D1{
 	int instance = 2;
 	
