8c06c0da1449ec77849f1f9c84890e52d1a02a40
devyoseph <89521661+devyoseph@users.noreply.github.com>
2021-09-02 17:31:01 +0900

    Add files via upload

---
+++ Oversuper.java
@@ -0,0 +1,17 @@
+package Over;
+
+class Calculator{
+	int a; int b;
+	
+	public void sum() {
+		System.out.println(a+b);
+	}	
+}
+public class Oversuper {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
