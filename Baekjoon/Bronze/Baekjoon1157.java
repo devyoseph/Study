46bf2fb156cc40e1d4b72f64d69be6fa44e19cdc
devyoseph <89521661+devyoseph@users.noreply.github.com>
2021-10-10 05:40:28 +0900

    Update Baekjoon1157.java

--- Baekjoon/Bronze/Baekjoon1157.java
+++ Baekjoon/Bronze/Baekjoon1157.java
@@ -38,3 +38,43 @@ private static int countChar(String s, char c) {
 	return count;
 }
 }
+
+
+//
+// import java.io.*;
+// public class Main {
+// public static void main(String[] args) throws IOException {
+// 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 	   int[] arr = new int[26];
+// 	   int max = 0;
+// 	   char ch ='?';
+// 	   int count = 0;
+// 	   String s = br.readLine().toUpperCase();
+// 	   for(int i =0; i<26; i++) {
+// 		   arr[i] = countChar(s,(char)(65+i));
+// 	   }
+// 	   for(int i = 0; i<26; i++) {
+// 		   if(arr[i]>max) {
+// 			   max = arr[i];
+// 			   ch = (char)(65+i);
+// 			   count = 0;
+// 		   }
+// 		   if(arr[i]==max) {
+// 			   count++;
+// 		   }
+// 	   }
+// 	   if(count ==1) {
+// 		   System.out.println(ch);
+// 	   }else { System.out.println('?');}
+// 	}
+
+// private static int countChar(String s, char c) {
+// 	int count = 0;
+// 	for(int i =0; i<s.length(); i++) {
+// 		if(s.charAt(i)==c) {
+// 			count++;
+// 		}
+// 	}
+// 	return count;
+// }
+// }
