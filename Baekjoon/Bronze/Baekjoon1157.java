f186bfbe3d4170d1f49547dd439b2a2153fe6a73
devyoseph <89521661+devyoseph@users.noreply.github.com>
2021-10-10 05:26:17 +0900

    Create Baekjoon1157.java

---
+++ Baekjoon/Bronze/Baekjoon1157.java
@@ -0,0 +1,42 @@
+import java.io.*;
+
+public class Main {
+public static void main(String[] args) throws IOException {
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	   BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//	   StringBuilder sb = new StringBuilder();
+//	   StringTokenizer st;
+	   int[] arr = new int[26];
+	   int max = 0;
+	   char ch ='?';
+	   int count = 0;
+	   String s = br.readLine().toUpperCase();
+	   for(int i =0; i<26; i++) {
+		   arr[i] = countChar(s,(char)(65+i));
+	   }
+	   for(int i = 0; i<26; i++) {
+		   if(arr[i]>max) {
+			   max = arr[i];
+			   ch = (char)(65+i);
+		   }
+	   }
+	   for(int i =0; i<26; i++) {
+		   if(max == arr[i]) {
+			   count ++;
+		   }
+	   }
+	   if(count ==1) {
+		   System.out.println(ch);
+	   }else { System.out.println("?");}
+	}
+
+private static int countChar(String s, char c) {
+	int count = 0;
+	for(int i =0; i<s.length(); i++) {
+		if(s.charAt(i)==c) {
+			count++;
+		}
+	}
+	return count;
+}
+}
