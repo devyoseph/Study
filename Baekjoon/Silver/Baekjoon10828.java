1de07d741b26fe35218201fb1f8242abc937dec1
devyoseph <89521661+devyoseph@users.noreply.github.com>
2021-10-20 04:31:01 +0900

    Update Baekjoon10828.java

--- Baekjoon/Silver/Baekjoon10828.java
+++ Baekjoon/Silver/Baekjoon10828.java
@@ -25,3 +25,22 @@ public static void main(String[] args) throws IOException{
 	 bw.close();
      //한번에 출력해도 정답으로 처리된다
 }}
+
+//스택 클래스를 활용한다
+// import java.io.*;
+// import java.util.Stack;
+// import java.util.StringTokenizer;
+// public class Main {
+// public static void main(String[] args) throws IOException{
+// 	 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+// 	 Stack<Integer> stack = new Stack<Integer>();
+// 	 StringTokenizer st;
+// 	 for(int i = Integer.parseInt(br.readLine()); i>0; i--) {
+// 		st = new StringTokenizer(br.readLine()," ");
+// 		switch(st.nextToken()) {
+// 		case"push": stack.push(Integer.parseInt(st.nextToken())); break;
+// 		case"pop": System.out.println(stack.isEmpty()?-1:stack.pop()); break;
+// 		case"top": System.out.println(stack.isEmpty()?-1:stack.peek()); break;
+// 		case"size": System.out.println(stack.size()); break;
+// 		case"empty": System.out.println(stack.isEmpty()?1:0); break;
+// }}}}
