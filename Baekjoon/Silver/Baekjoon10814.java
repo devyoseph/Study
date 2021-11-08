39ce9ed4df315f356d600ef3a5d0fab9025d1ee3
devyoseph <89521661+devyoseph@users.noreply.github.com>
2021-11-09 01:12:30 +0900

    Create Baekjoon10814.java

---
+++ Baekjoon/Silver/Baekjoon10814.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+public static void main(String[] args) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st;
+	List<String>[] arr = new ArrayList[201]; //201크기의 ArrayList 배열생성
+	for (int i = 1; i < 201; i++) {
+		arr[i] = new ArrayList<String>(); //배열 안에 재선언
+	}
+	int N=Integer.parseInt(br.readLine());
+    
+	for(int i=0;i<N;i++) {
+		st = new StringTokenizer(br.readLine());
+		int index=Integer.parseInt(st.nextToken()); //앞에 index를 받은 뒤
+		arr[index].add(st.nextToken()); //index에 해당하는 곳에 문자열 추가
+	}
+	for(int i=1;i<201;i++) {
+		for(int j=0;j<arr[i].size();j++) {
+			System.out.println(i+" "+arr[i].get(j)); //문자열을 꺼내준다
+		}
+	}
+}}
