6430e8d765d99e23cd989bd87626ac8060d0af4d
devyoseph <josephdev123@naver.com>
2021-12-14 19:32:45 +0900

    Create updateAndDelete.sql

---
+++ study039/updateAndDelete.sql
@@ -0,0 +1,6 @@
+UPDATE main.table_new SET name='맹구', age=8 WHERE id = 4;
+// id = 4인 row의 name값을 '맹구'로, age를 8로 업데이트합니다.
+
+UPDATE main.table_new SET age=age+10;
+
+DELETE FROM table_new WHERE id=1;
